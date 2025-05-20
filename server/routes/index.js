cde94655f6c4f1fc11c3afe66d4c64dc72c980d3
Maebh Collins <maebhcollins@gmail.com>
2025-05-20 18:43:55 +0100

    Add jobs API endpoints and docs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const jobsRouter = require('./jobs');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({ message: 'API working' });
 });
+
+router.use('/jobs', jobsRouter);
 
 module.exports = router;
